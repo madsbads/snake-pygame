1ec10980d1e6fb02e8d7dd72075dba8e6baa1255
Maddie Nevans <maddieelizabethtaylor@gmail.com>
2024-06-01 20:27:43 -0400

    player increases size and speed, score displayed

--- main.py
+++ main.py
@@ -1,78 +1,87 @@
 import pygame
+import random
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((800, 800))
-clock = pygame.time.Clock()
-player = pygame.Rect((200,200,25,25))
-target = pygame.Rect((300,300,25,25))
 running = True
+lastKey = None
+speed_level = 10
+SIZE = 800
+PXL = 25
+score = 0
+score_text = '0'
+font = pygame.font.Font(pygame.font.get_default_font(), 36)
+screen = pygame.display.set_mode((SIZE, SIZE))
+clock = pygame.time.Clock()
+player = pygame.Rect((random.randrange(SIZE-50),random.randrange(SIZE-50),PXL,PXL)) # starting position is random on screen with 50 px margin
+food = pygame.Rect((random.randrange(SIZE-50),random.randrange(SIZE-50),PXL,PXL)) # starting position is random on screen with 50 px margin
 
+# FUNCTIONS
+def generateMeal():
+    food = pygame.Rect((random.randrange(SIZE-50),random.randrange(SIZE-50),PXL,PXL)) # starting position is random on screen with 50 px margin
+    return food
+    
+def lunch(): 
+    if pygame.Rect.colliderect(player, food):
+        return True
+    else: return False
+
+def speedControl(current_speed):
+    speed = current_speed + 4
+    return speed
+
+def grow(player):
+    player.inflate_ip(PXL,0)
+    return player
+
+# GAME LOOP
 while running:
+    
+    # dec variables
+    key = pygame.key.get_pressed()
+    
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN: 
+            lastKey = event.key
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
-    
-    # draw player and target
-    pygame.draw.rect(screen, (250,0,0), player)
-    pygame.draw.rect(screen, (0,250,0), target)
+    text_surface = font.render(score_text, True, (0, 0, 250))
+    screen.blit(text_surface, dest=(0,0))
 
     # RENDER YOUR GAME HERE
     # directional inputs
-    key = pygame.key.get_pressed()
-    if key[pygame.K_LEFT] == True:
-        player.x -= 4
-    elif key[pygame.K_RIGHT] == True:
-        player.x += 4
-    elif key[pygame.K_UP] == True:
-        player.y -= 4
-    elif key[pygame.K_DOWN] == True:
-        player.y += 4
-
+    if lastKey == pygame.K_LEFT:
+         player.x -= speed_level
+    elif lastKey == pygame.K_RIGHT:
+        player.x += speed_level
+    elif lastKey == pygame.K_UP:
+        player.y -= speed_level
+    elif lastKey == pygame.K_DOWN:
+        player.y += speed_level
+    
+    # detect meal
+    eat = lunch()
+    if eat:
+        food = generateMeal()
+        score += 1
+        score_text = str(score)
+        player = grow(player)
+        if score % 5 == 0: 
+            speed_level = speedControl(speed_level) # speed of player increases as score climbs; 5 level step
+        
+    # draw player and target
+    pygame.draw.rect(screen, (250,0,0), player)
+    pygame.draw.rect(screen, (0,250,0), food)
     # flip() the display to put your work on screen
     pygame.display.flip()
 
     # fps
     clock.tick(30)  # this controls the speed of the game. low values makes the game slower, and large values makes the game faster.
 
+# quit when game loop is exited
 pygame.quit()
-
-
-
-# import pygame
-# pygame.init()
-
-# screen = pygame.display.set_mode((800, 800))
-# clock = pygame.time.Clock()
-# player = pygame.Rect((200,200,25,25))
-# run = True
-
-# while run:
-    
-#     screen.fill((0,0,0))
-    
-#     pygame.draw.rect(screen, (250,0,0), player)
-    
-#     key = pygame.key.get_pressed()
-#     if key[pygame.K_LEFT] == True:
-#         player.move_ip(-1,0)
-#     elif key[pygame.K_RIGHT] == True:
-#         player.move_ip(1,0)
-#     elif key[pygame.K_UP] == True:
-#         player.move_ip(0,-1)
-#     elif key[pygame.K_DOWN] == True:
-#         player.move_ip(0,1)
-    
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT: 
-#             run = False
-
-#     pygame.display.update()
-#     clock.tick(60)
-    
-# pygame.quit()
